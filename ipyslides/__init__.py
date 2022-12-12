32831d74aded403f3e23f312612222fe5b386132
massgh <massgh@users.noreply.github.com>
2022-12-12 13:55:13 -0500

    Layout CSS Completed, 2.2.1

--- ipyslides/__init__.py
+++ ipyslides/__init__.py
@@ -7,7 +7,7 @@ from .core import Slides
 __all__ = ['Slides']
 
 major, minor, patch = [int(v) for v in __version__.split('.')]
-if major < 3 and minor < 2:
+if major < 3 and minor < 3:
     print('Starting from version 2.0, there is complete rewrite of themeing, so custom CSS classes may not work! See `Slides.docs()` for more info.')
 
 if __name__ == '__main__':
