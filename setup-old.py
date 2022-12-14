d3258d2dfb6f8e65bcd988c2a85162698f010739
massgh <massgh@users.noreply.github.com>
2022-12-14 18:16:22 -0500

    Testing New Setup.py

---
+++ setup-old.py
@@ -0,0 +1,34 @@
+import setuptools
+from ipyslides.__version__ import __version__
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ipyslides",
+    version = __version__,
+    author = "Abdul Saboor",
+    author_email = "mass_qau@outlook.com",
+    description = "Live rich content slides in jupyter notebook",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/massgh/ipyslides",
+    project_urls = {
+        "Bug Tracker": "https://github.com/massgh/ipyslides/issues",
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    requires = ['markdown','ipywidgets'],
+    install_requires = [
+          'markdown',
+          'markdown-customblocks',
+          'ipywidgets'
+      ],
+    packages = setuptools.find_packages(),
+    keywords        = ['Jupyter', 'Widgets', 'IPython'],
+    include_package_data = True,
+    python_requires =">=3.6",
+)
--- setup.py
+++ setup.py
@@ -1,34 +1,121 @@
-import setuptools
-from ipyslides.__version__ import __version__
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+import os, sys
+from shutil import rmtree
 
-setuptools.setup(
-    name="ipyslides",
-    version = __version__,
-    author = "Abdul Saboor",
-    author_email = "mass_qau@outlook.com",
-    description = "Live rich content slides in jupyter notebook",
+from setuptools import setup, Command
+
+# Package meta-data.
+NAME = 'ipyslides'
+DESCRIPTION = 'Live rich content slides in jupyter notebook'
+URL = 'https://github.com/massgh/ipyslides'
+EMAIL = 'mass_qau@outlook.com'
+AUTHOR = 'Abdul Saboor'
+REQUIRES_PYTHON = '>=3.7'
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    'markdown', 'markdown-customblocks', 'ipywidgets'
+]
+
+# What packages are optional?
+EXTRAS = {
+    'extra': ['jupyterlab==3.3.0','ipython==7.25.0'],
+}
+
+KEYWORDS = ['Jupyter', 'Widgets', 'IPython']
+PROJECT_URLS = {
+    "Bug Tracker": "https://github.com/massgh/ipyslides/issues",
+}
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+with open(os.path.join(here, NAME, '__version__.py')) as f:
+        exec(f.read(), about)
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        print(f'\n\033[1;92m{s}\033[0m\n{"-" * 50}')
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds …')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution …')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine …')
+        status_code = os.system('twine upload dist/*')
+        if status_code != 0:
+            sys.exit(status_code)
+            
+        yes_no = input('Upload this version to GitHub? [y/n]: ')
+        if yes_no.lower() == 'y':
+            self.status('Pushing git tags…')
+            os.system('git tag v{0}'.format(about['__version__']))
+            os.system('git push --tags')
+
+        sys.exit()
+
+
+# Where the magic happens:
+setup(
+    name = NAME,
+    version = about['__version__'],
+    description = DESCRIPTION,
     long_description = long_description,
-    long_description_content_type = "text/markdown",
-    url = "https://github.com/massgh/ipyslides",
-    project_urls = {
-        "Bug Tracker": "https://github.com/massgh/ipyslides/issues",
-    },
-    classifiers = [
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+    long_description_content_type = 'text/markdown',
+    author = AUTHOR,
+    author_email = EMAIL,
+    python_requires = REQUIRES_PYTHON,
+    url = URL,
+    #packages = find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    # If your package is a single module, use this instead of 'packages':
+    py_modules=['ipyslides'],
+
+    # entry_points={
+    #     'console_scripts': ['mycli=mymodule:cli'],
+    # },
+    install_requires = REQUIRED,
+    extras_require = EXTRAS,
+    include_package_data = True,
+    license = 'MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
         "Operating System :: OS Independent",
     ],
-    requires = ['markdown','ipywidgets'],
-    install_requires = [
-          'markdown',
-          'markdown-customblocks',
-          'ipywidgets'
-      ],
-    packages = setuptools.find_packages(),
-    keywords        = ['Jupyter', 'Widgets', 'IPython'],
-    include_package_data = True,
-    python_requires =">=3.6",
+    keywords = KEYWORDS,
+    project_urls = PROJECT_URLS,
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
